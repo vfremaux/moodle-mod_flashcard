5eee2c7985f44f5ab43c5f8d4252b0ee08fcfd23
valery <valery.fremaux@gmail.com>
2017-02-22 12:19:50 +0100

    Fix backup abstract cleanup error

--- backup/moodle2/restore_flashcard_activity_task.class.php
+++ backup/moodle2/restore_flashcard_activity_task.class.php
@@ -28,6 +28,7 @@ require_once($CFG->dirroot.'/mod/flashcard/backup/moodle2/restore_flashcard_step
 class restore_flashcard_activity_task extends restore_activity_task {
 
     protected function define_my_settings() {
+        assert(1);
     }
 
     protected function define_my_steps() {
