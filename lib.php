6f29b4a0be7a19ad88ef8f5aaa76f0f491330ada
valery <valery.fremaux@gmail.com>
2019-12-09 18:09:25 +0100

    Automated sync from SVN

--- lib.php
+++ lib.php
@@ -113,6 +113,10 @@ function flashcard_add_instance($flashcard) {
         flashcard_save_draft_customimage($flashcard, $ci);
     }
 
+    // weird hack
+    // Quickform fails getting thios value in $data.
+    $flashcard->completionallviewed = clean_param($_POST['completionallviewed'], PARAM_INT);
+
     $newid = $DB->insert_record('flashcard', $flashcard);
 
     // Import all information from question.
@@ -158,6 +162,10 @@ function flashcard_update_instance($flashcard) {
     foreach ($customimages as $ci) {
         flashcard_save_draft_customimage($flashcard, $ci);
     }
+
+    // weird hack
+    // Quickform fails getting thios value in $data.
+    $flashcard->completionallviewed = clean_param($_POST['completionallviewed'], PARAM_INT);
 
     $return = $DB->update_record('flashcard', $flashcard);
 
