41d566e143c2673db796c7e411a95a11de8e4b76
Valery Fremaux <valery.fremaux@gmail.com>
2025-04-24 16:22:41 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -28,12 +28,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019121103;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2025011400;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2022112801;  // Requires this Moodle version (3.6 and above).
 $plugin->component = 'mod_flashcard'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '4.1.0 (Build 2019121103)';
+$plugin->release = '4.5.0 (Build 2019121103)';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->supported = [401, 402];
+$plugin->supported = [401, 405];
 
 // Non Moodle attributes.
-$plugin->codeincrement = '4.1.0005';
+$plugin->codeincrement = '4.5.0005';
